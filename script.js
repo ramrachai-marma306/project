c6f236f7647a6de53da0b3d927c2962f6d48622e
Ramrachai <ramrasay940@gmail.com>
2026-02-02 15:49:15 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showLogin(){
+    document.querySelector('.signup').classList.remove('active');
+    document.querySelector('.login').classList.add('active');
+}
+
+function showSignup(){
+    document.querySelector('.login').classList.remove('active');
+    document.querySelector('.signup').classList.add('active');
+}
